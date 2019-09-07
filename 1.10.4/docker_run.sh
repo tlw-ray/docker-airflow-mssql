2ed635e2716c61875df9d076ba7dde5962a807ff
tlw-ray <tlw_ray@163.com>
2019-09-08 02:19:30 +0800

    update

--- 1.10.4/docker_run.sh
+++ 1.10.4/docker_run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+docker pull tlwtlw/docker-airflow:1.10.4-mssql
 docker run \
 --name airflow \
 -d \
@@ -7,11 +8,11 @@ docker run \
 -e TZ=Asia/Shanghai \
 -e LOAD_EX=y \
 -e EXECUTOR=Local \
--e DAG_DIR_LIST_INTERVAL=30 \
 -e MSSQL_HOST=192.168.0.120 \
 -e MSSQL_USER=sa \
 -e MSSQL_PASSWORD=@Welcome120 \
 -e AIRFLOW__SQL_ALCHEMY_SCHEMA= \
+-e AIRFLOW__SCHEDULER_DAG_DIR_LIST_INTERVAL=30 \
 -v "${PWD}/../dags/:/usr/local/airflow/dags/" \
 -v "${PWD}/airflow.cfg:/usr/local/airflow/airflow.cfg" \
 -v "${PWD}/entrypoint.sh:/entrypoint.sh" \
