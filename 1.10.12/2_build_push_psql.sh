6fe9d3d7099b6b700a10c2ee2288f7fda31dddcd
tlw-ray <tlw_ray@163.com>
2020-09-11 15:30:27 +0800

    updaet

--- 1.10.12/2_build_push_psql.sh
+++ 1.10.12/2_build_push_psql.sh
@@ -1,2 +1,2 @@
-docker build -f DockerfileTest . -t tlwtlw/docker-airflow:1.10.12-psql
+docker build -f DockerfilePSQL . -t tlwtlw/docker-airflow:1.10.12-psql
 docker push tlwtlw/docker-airflow:1.10.12-psql
