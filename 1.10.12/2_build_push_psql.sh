fd2accd86357af75de0ffb7a47a6267bf6e28018
tlw-ray <tlw_ray@163.com>
2020-09-11 15:51:26 +0800

    update

--- 1.10.12/2_build_push_psql.sh
+++ 1.10.12/2_build_push_psql.sh
@@ -1,2 +1,2 @@
-docker build -f DockerfilePSQL . -t tlwtlw/docker-airflow:1.10.12-psql
-docker push tlwtlw/docker-airflow:1.10.12-psql
+docker build -f DockerfilePSQL . -t tlwtlw/docker-airflow:1.10.12.1-psql
+docker push tlwtlw/docker-airflow:1.10.12.1-psql
--- 1.10.12/4_company_pull_push_mssql.bat
+++
@@ -1,3 +0,0 @@
-docker pull tlwtlw/docker-airflow:1.10.12-mssql
-docker tag tlwtlw/docker-airflow:1.10.12-mssql winning.repo/dcs/docker-airflow:1.10.12-mssql
-docker push winning.repo/dcs/docker-airflow:1.10.12-mssql
